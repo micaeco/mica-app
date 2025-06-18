493aaa62210bdb28a6f60991d9f0942b2346937e
micaeco-admin <admin@mica.eco>
2025-06-18 19:49:16 +0200

    fix: correct NODE_ENV assignment and ensure strict equality check in env configuration

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    NODE_ENV: z.enum(["development", "test", "production"]),
     AUTH0_SECRET: z.string(),
     AUTH0_CLIENT_ID: z.string(),
     AUTH0_DOMAIN: z.string().url(),
@@ -14,9 +15,9 @@ export const env = createEnv({
   },
 
   runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV ?? "development",
     NEXT_PUBLIC_URL:
-      process.env.NODE_ENV == "production" ? "https://app.mica.eco" : "http://localhost:3000",
-
+      process.env.NODE_ENV === "production" ? "https://app.mica.eco" : "http://localhost:3000",
     AUTH0_SECRET: process.env.AUTH0_SECRET,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
