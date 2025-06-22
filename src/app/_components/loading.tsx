31afeff6359ed6ca0a5bb5a71d5b6be71e63a7c4
micaeco-admin <admin@mica.eco>
2025-06-22 16:44:57 +0200

    fix: move animation class to LoaderCircle for better structure

--- src/app/_components/loading.tsx
+++ src/app/_components/loading.tsx
@@ -2,8 +2,8 @@ import { LoaderCircle } from "lucide-react";
 
 export function Loading() {
   return (
-    <div className="flex h-screen w-full animate-spin items-center justify-center">
-      <LoaderCircle size={64} />
+    <div className="flex h-screen w-full items-center justify-center">
+      <LoaderCircle size={64} className="animate-spin" />
     </div>
   );
 }
