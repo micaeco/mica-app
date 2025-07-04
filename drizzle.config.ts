80391aafe67f85a3cc9517ed67c0e7cb5ae541a3
micaeco-admin <admin@mica.eco>
2025-07-04 16:03:12 +0200

    refactor: move migrations from drizzle root folder to the db folder inside infrastructure

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,7 +7,7 @@ config({ path: path.resolve(__dirname, ".env.development.local") });
 
 export default defineConfig({
   schema: "./src/infrastructure/db/schema/**/*.ts",
-  out: "./drizzle",
+  out: "./src/infrastructure/db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
