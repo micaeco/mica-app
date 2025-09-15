0a4af188c92cf0c65674984fd366031e6808343f
micaeco-admin <admin@mica.eco>
2025-09-15 21:48:48 +0200

    Add new logo image (logo-512.png) to public/icons directory

--- src/app/_components/base-layout.tsx
+++ src/app/_components/base-layout.tsx
@@ -20,6 +20,17 @@ export async function BaseLayout({
     <html lang={locale}>
       <head>
         <meta name="apple-mobile-web-app-title" content="MICA" />
+        <meta name="apple-mobile-web-app-title" content="MICA" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/logo-180.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/logo-167.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/logo-120.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/logo-152.png" />
+
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className={montserrat.className}>
         <ClientProviders locale={locale} messages={messages}>
